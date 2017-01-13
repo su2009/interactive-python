71f27fa9b728f1e37b9948e139753c9e3ab6f65d
su2009 <yangsu2009@gmail.com>
2017-01-13 12:53:56 -0800

    Update rock_paper_game.py

--- rock_paper_game.py
+++ rock_paper_game.py
@@ -18,6 +18,22 @@ def name_to_number(name):
       
 def number_to_name(number):
     if number == 0:
+def name_to_number(name):
+    if name == 'rock':
+       return 0
+    if name == 'spock':
+       return 1
+    if name == 'paper':
+       return 2
+    if name == 'lizard':
+       return 3
+    if name == 'scissors':
+       return 4
+      
+      
+      
+def number_to_name(number):
+    if number == 0:
        return 'rock'
     if number == 1:
        return 'spock'
@@ -29,8 +45,8 @@ def number_to_name(number):
        return 'scissors'
 
 
-def rpsls(user_name):
-    user_num = name_to_number(user_name)
+def rpsls(choice):
+    user_num = name_to_number(choice)
     pc_num = random.randrange(0,5)
     pc_name = number_to_name(pc_num)
     dif = user_num - pc_num
@@ -38,28 +54,31 @@ def rpsls(user_name):
     if dif == 0:
        print "equal"
     elif 0 < dif <= 2:
-       print "Player chooses %s" %user_name
+       print "Player chooses %s" %choice
        print "Computer chooses %s" %pc_name
        print "Player wins!"
     elif dif<= -3:
-       print "Player chooses %s" %user_name
+       print "Player chooses %s" %choice
        print "Computer chooses %s" %pc_name
        print "Player wins!"
     else:
-       print "Player chooses %s" %user_name
+       print "Player chooses %s" %choice
        print "Computer chooses %s" %pc_name
        print "Computer wins!"
 
 
 print "Let's play the rpsls game. Please input your choice - rock, spock, paper, lizard,or scissors?"
 user_name = raw_input(">")
-for user_name in ('rock','spock','paper','lizard','scissors'):
-    rpsls(user_name)
-    exit()
-else: 
-    print "You input is %, not in our lists." %user_name
+print user_name
+list = ['rock','spock','paper','lizard','scissors']
+while user_name not in list:
+    print "You input is %s, not in our lists." %user_name
     print "Please input your choice - rock, spock, paper, lizard,or scissors?"
     user_name = raw_input(">")
+else: 
+    rpsls(user_name)
+    exit()
+        
     
     
     
